cfcc7155c4a8a428967b51eb7a558ea0ec0ae762
king chan <bassking123456@yahoo.com.hk>
2022-04-06 12:12:11 -0500

    fixed the non-login user Null loginName error

--- authen.php
+++ authen.php
@@ -2,6 +2,7 @@
     session_start();
 
     $role = '';
+    $userLoginId = '';
     if(isset($_SESSION['sess_member_id']) && $_SESSION['sess_member_id'] != "") {
         echo '<h1>Welcome '.$_SESSION['sess_login_name'].', your ID is '.$_SESSION['sess_member_id'].'</h1>';
         $role = $_SESSION['sess_role'];
